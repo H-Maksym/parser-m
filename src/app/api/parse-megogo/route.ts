001508628688ed89b6f462b08541633fe8d248d3
Maksym-Holovachuk <mgolovachuk@gmail.com>
2025-09-19 03:27:33 +0300

    change @sparticuz/chromium-min

--- src/app/api/parse-megogo/route.ts
+++ src/app/api/parse-megogo/route.ts
@@ -1,4 +1,4 @@
-import chromium from '@sparticuz/chromium';
+import chromium from '@sparticuz/chromium-min';
 import { NextRequest, NextResponse } from 'next/server';
 import puppeteer from 'puppeteer-core';
 

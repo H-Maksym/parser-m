86c20e6b38619051ddad6809722a9586a66c9c0d
Maksym-Holovachuk <mgolovachuk@gmail.com>
2025-11-20 00:19:44 +0200

    testing ua proxy

--- src/app/api/parse-megogo/config/puppeteer-config.ts
+++ src/app/api/parse-megogo/config/puppeteer-config.ts
@@ -70,6 +70,20 @@ export async function launchBrowser() {
   });
 
   await page.evaluateOnNewDocument(() => {
+    // @ts-expect-error mock chrome.runtime for tests
+    window.googletag = {
+      cmd: [],
+      pubads: () => ({
+        enableSingleRequest() {},
+        collapseEmptyDivs() {},
+        setCentering() {},
+        addEventListener() {},
+      }),
+      enableServices() {},
+    };
+  });
+
+  await page.evaluateOnNewDocument(() => {
     const realGeo = 'ua';
 
     // Патчимо getAttribute
@@ -95,47 +109,6 @@ export async function launchBrowser() {
       }
     }, 1);
   });
-
-  await page.evaluateOnNewDocument(() => {
-    // @ts-expect-error mock chrome.runtime for tests
-    window.googletag = {
-      cmd: [],
-      pubads: () => ({
-        enableSingleRequest() {},
-        collapseEmptyDivs() {},
-        setCentering() {},
-        addEventListener() {},
-      }),
-      enableServices() {},
-    };
-  });
-
-  // await page.evaluateOnNewDocument(() => {
-  //   const realGeo = 'ua';
-
-  //   // Патчимо getAttribute
-  //   const origGetAttr = Element.prototype.getAttribute;
-  //   Element.prototype.getAttribute = function (name) {
-  //     if (name === 'data-geo') return realGeo;
-  //     return origGetAttr.call(this, name);
-  //   };
-
-  //   // Патчимо setAttribute
-  //   const origSetAttr = Element.prototype.setAttribute;
-  //   Element.prototype.setAttribute = function (name, value) {
-  //     if (name === 'data-geo') return; // блокуємо перезапис
-  //     return origSetAttr.call(this, name, value);
-  //   };
-
-  //   // Чекаємо появи <html>
-  //   const interval = setInterval(() => {
-  //     const html = document.documentElement;
-  //     if (html) {
-  //       html.setAttribute('data-geo', realGeo);
-  //       clearInterval(interval);
-  //     }
-  //   }, 1);
-  // });
 
   await page.evaluateOnNewDocument(() => {
     Object.defineProperty(navigator, 'webdriver', { get: () => false });
